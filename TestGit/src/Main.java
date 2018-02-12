1d9f1815ec0e177c7b67a88454d42b3c90b3ef6e
Ragavendira <ragavendira.rajathirumalai>
2018-02-12 06:21:38 -0500

    first commit

---
+++ TestGit/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		Main.displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting() {
+		// this is checking 
+		System.out.println("Hello happy sales people");
+		System.out.println("This app shows sale data");
+		System.out.println("-------------------------");
+		// this is checking 4
+		System.out.println("Test 1");
+		System.out.println("Test 2");
+		// added new line into main code
+		System.out.println("Test 3");
+		// added new line into main code
+		System.out.println("Test 4");
+		// added new line into main code
+		System.out.println("Test 5");
+		// added new line into main code
+		System.out.println("Test 6.1");
+											
+	}
+}
---
+++ TestGit/src/SalesData.java
@@ -0,0 +1,11 @@
+public class SalesData {
+	
+	int[] data = {-1, 0, 4, 42, 84};
+	
+	public void display() {
+		System.out.println("Data: ");
+		for(int i : data) {
+			System.out.println("Next value: " + data[i]);
+		}
+	}
+}
